b28929619dc5815e0d1ad0a1f04aea43c5d13a8e
KERA_ <KERA_@JDONOSOR114>
2020-12-01 21:35:15 -0300

    this is a test

---
+++ src/empresa/Principal.java
@@ -0,0 +1,26 @@
+//Integrantes: Jaime Donoso, Rafael Calderón, Ignacio Caro, Calor Llanca
+
+package empresa;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+
+		Scanner sc = new Scanner(System.in);
+
+		Menu menu = new Menu();
+
+		int opcion = 0;
+		do { 
+			opcion = menu.menu();
+		}
+		while ( opcion != 9);
+
+		sc.close();
+
+	}
+
+}
